9c6553f12ba94ff6efb31ebdbe78adb2e98213f0
Carlos Duarte Rodríguez <130692626+CarlosDuarteRod@users.noreply.github.com>
2023-06-16 19:32:15 +0000

    Proyecto final

--- src/app.js
+++ src/app.js
@@ -5,8 +5,9 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function onLoad() {
+window.onload = function() {
   //write your code here
+
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
@@ -18,19 +19,24 @@ window.onload = function onLoad() {
     "while I was praying"
   ];
 
-  let aleatorio1 = Math.floor(Math.random() * who.length);
-  let array1 = who[aleatorio1];
+  function onLoad(who, action, what, when) {
+    let aleatorio1 = Math.floor(Math.random() * who.length);
+    let array1 = who[aleatorio1];
 
-  let aleatorio2 = Math.floor(Math.random() * action.length);
-  let array2 = action[aleatorio2];
+    let aleatorio2 = Math.floor(Math.random() * action.length);
+    let array2 = action[aleatorio2];
 
-  let aleatorio3 = Math.floor(Math.random() * what.length);
-  let array3 = what[aleatorio3];
+    let aleatorio3 = Math.floor(Math.random() * what.length);
+    let array3 = what[aleatorio3];
 
-  let aleatorio4 = Math.floor(Math.random() * when.length);
-  let array4 = when[aleatorio4];
+    let aleatorio4 = Math.floor(Math.random() * when.length);
+    let array4 = when[aleatorio4];
 
-  let concatenacionarray = array1 + " " + array2 + " " + array3 + " " + array4;
+    let concatenacionarray =
+      array1 + " " + array2 + " " + array3 + " " + array4;
 
-  document.getElementById("excuse").innerHTML = concatenacionarray;
+    return concatenacionarray;
+  }
+  let totalconcatenacion = onLoad(who, action, what, when);
+  document.getElementById("excuse").innerHTML = totalconcatenacion;
 };
