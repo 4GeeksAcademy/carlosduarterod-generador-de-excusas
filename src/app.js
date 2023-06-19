4dc44b33ef4740d3014457b50d27cc5e0792ebe2
Carlos Duarte Rodríguez <130692626+CarlosDuarteRod@users.noreply.github.com>
2023-06-19 18:09:36 +0000

    Proyecto final

--- src/app.js
+++ src/app.js
@@ -11,15 +11,10 @@ window.onload = function() {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+  let when = [ "before the class", "right on time", "when I finished", "during my lunch", "while I was praying" ];
 
-  function onLoad(who, action, what, when) {
+  function onLoad (who, action, what, when) {
+
     let aleatorio1 = Math.floor(Math.random() * who.length);
     let array1 = who[aleatorio1];
 
@@ -32,8 +27,7 @@ window.onload = function() {
     let aleatorio4 = Math.floor(Math.random() * when.length);
     let array4 = when[aleatorio4];
 
-    let concatenacionarray =
-      array1 + " " + array2 + " " + array3 + " " + array4;
+    let concatenacionarray = array1 + " " + array2 + " " + array3 + " " + array4;
 
     return concatenacionarray;
   }
